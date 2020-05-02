696592d280670e9e4935cddf083d9ddf9435a5d1
mickaelbalensi <mickaelbalensi2652@gmail.com>
2020-05-02 22:25:44 +0300

    2 test de la sphere reussi

--- src/Test/elementTest/IntegrationTest.java
+++ src/Test/elementTest/IntegrationTest.java
@@ -53,13 +53,13 @@ public class IntegrationTest {
 
         //region TC03 : The Sphere is in front of the screen but don't fill it
 
-        Sphere sphere3 = new Sphere(new Point3D(0,0,2),2 );
+        Sphere sphere3 = new Sphere(new Point3D(0,0,2.5),2 );
 
         int count3 = 0;
 
         for(int i=0;i<Nx;i++)
             for(int j=0;j<Ny;j++){
-                Ray ray= cam.constructRayThroughPixel(Nx,Ny,j,i,1,3,3);
+                Ray ray= cam2.constructRayThroughPixel(Nx,Ny,j,i,1,3,3);
                 List<Point3D> result= sphere3.findIntersections(ray);
                 if(result != null)
                     count3+= result.size();
@@ -86,7 +86,7 @@ public class IntegrationTest {
 
         //region TC05 : The Sphere is behind the camera
 
-        Sphere sphere5 = new Sphere(new Point3D(0,0,1),0.5 );
+        Sphere sphere5 = new Sphere(new Point3D(0,0,-1),0.5 );
 
         int count5 = 0;
 
@@ -106,7 +106,7 @@ public class IntegrationTest {
     void constructRayThroughPixelWithPlane(){
 
         //region TC01 : The Plane is behind the screen
-        Plane plane1 = new Plane(new Vector(0,0,-1), new Point3D(0,0,-5));
+        Plane plane1 = new Plane(new Vector(0,0,1), new Point3D(0,0,5));
 
         int count1 = 0;
 
@@ -122,13 +122,13 @@ public class IntegrationTest {
         //endregion
 
         //region TC02 : The Plane is parallel to the screen
-        Plane plane2 = new Plane(new Vector(2,2,-2), new Point3D(0,0,-5));
+        Plane plane2 = new Plane(new Point3D(1.5,-1.5,1),new Point3D(-1.5,-1.5,1),new Point3D(0,2,2));
 
         int count2 = 0;
 
         for(int i=0;i<Nx;i++)
             for(int j=0;j<Ny;j++){
-                Ray ray= cam.constructRayThroughPixel(Nx,Ny,j,i,1,9,9);
+                Ray ray= cam.constructRayThroughPixel(Nx,Ny,j,i,1,3,3);
                 List<Point3D> result= plane2.findIntersections(ray);
                 if(result != null)
                     count2+= result.size();
@@ -138,7 +138,7 @@ public class IntegrationTest {
         //endregion
 
         //region TC03 : The Plane is oblique
-        Plane plane3 = new Plane(new Vector(0,4.5,-1), new Point3D(0,0,-5));
+        Plane plane3 = new Plane(new Vector(0,4.5,1), new Point3D(0,0,5));
 
         int count3 = 0;
 
@@ -173,13 +173,13 @@ public class IntegrationTest {
         //endregion
 
         //region TC02 : The big triangle is behind the screen
-        Triangle triangle2 = new Triangle(new Point3D(0,-1,2),new Point3D(1,1,2),new Point3D(-1,1,2));
+        Triangle triangle2 = new Triangle(new Point3D(0,-20,2),new Point3D(1,1,2),new Point3D(-1,1,2));
 
         int count2 = 0;
 
         for(int i=0;i<Nx;i++)
             for(int j=0;j<Ny;j++){
-                Ray ray= cam.constructRayThroughPixel(Nx,Ny,j,i,1,9,9);
+                Ray ray= cam.constructRayThroughPixel(Nx,Ny,j,i,1,3,3);
                 List<Point3D> result= triangle2.findIntersections(ray);
                 if(result != null)
                     count2+= result.size();
