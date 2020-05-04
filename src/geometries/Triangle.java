2dc7b98b856f57a2ec84e50c71440fdca0214deb
mickaelbalensi <mickaelbalensi2652@gmail.com>
2020-05-04 14:43:30 +0300

    recitation 3

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -26,6 +26,7 @@ public class Triangle extends Polygon {
     @Override
     public List<Point3D> findIntersections(Ray ray) {
         List<Point3D> intersections = _plane.findIntersections(ray);
+
         if (intersections == null) return null;
 
         Point3D pt = ray.getPt();
