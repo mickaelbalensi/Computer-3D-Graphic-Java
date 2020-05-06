c4ef591877f012d9c866e25502c5efb4667cc420
gCKn <bibasitshak@gmail.com>
2020-05-06 14:29:09 +0200

    test image writer fini avec photo

--- src/Test/RendererTest/ImageWriterTest.java
+++ src/Test/RendererTest/ImageWriterTest.java
@@ -12,19 +12,19 @@ public class ImageWriterTest {
 
     @Test
     public void ImageTest() {
-        ImageWriter newImage = new ImageWriter("newImage", 1000, 1600, 1000, 1600);
+        ImageWriter myImage1 = new ImageWriter("myImage1", 10, 16, 1000, 1600);
         {
            for (int h=0;h<1600;h++)
            {
                for (int w=0;w<1000;w++) {
-                   if (h % 10 != 0 && w % 10 != 0)
-                       newImage.writePixel(w, h, java.awt.Color.BLACK);
+                   if (h % 100 != 0 && w % 100 != 0)
+                       myImage1.writePixel(w, h, java.awt.Color.BLACK);
                    else
-                       newImage.writePixel(w, h, Color.WHITE);
+                       myImage1.writePixel(w, h, Color.WHITE);
                    }
                }
             }
-            newImage.writeToImage();
+            myImage1.writeToImage();
         }
     }
 
