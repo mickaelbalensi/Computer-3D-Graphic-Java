d563e14526a04f294d10f24d2d2fe25cd5d01f6a
gCKn <bibasitshak@gmail.com>
2020-05-10 15:14:40 +0200

    commentaires fini

--- src/Test/RendererTest/ImageWriterTest.java
+++ src/Test/RendererTest/ImageWriterTest.java
@@ -12,19 +12,19 @@ public class ImageWriterTest {
 
     @Test
     public void ImageTest() {
-        ImageWriter myImage2 = new ImageWriter("myImage2", 16, 10, 1600, 1000);
+        ImageWriter myImage = new ImageWriter("myImage", 16, 10, 1600, 1000);
         {
            for (int h=0;h<1000;h++)
            {
                for (int w=0;w<1600;w++) {
                    if (h % 100 != 0 && w % 100 != 0)
-                       myImage2.writePixel(w, h, java.awt.Color.BLACK);
+                       myImage.writePixel(w, h, java.awt.Color.BLACK);
                    else
-                       myImage2.writePixel(w, h, Color.WHITE);
+                       myImage.writePixel(w, h, Color.WHITE);
                    }
                }
             }
-            myImage2.writeToImage();
+            myImage.writeToImage();
         }
     }
 
